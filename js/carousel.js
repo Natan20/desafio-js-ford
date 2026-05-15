fe225952676232bd03e078261c2a0a3225cc599d
Natan <natanaelsantossi@hotmail.com>
2026-05-15 14:23:29 -0300

    Botões e Carousel Finalizados

--- js/carousel.js
+++ js/carousel.js
@@ -28,6 +28,13 @@ class Carousel {
                 Carousel._size = arr.length; // Guarda o tamanho total do array
                 Carousel.Next(); //start // Exibe o primeiro item imediatamente
                 Carousel._interval = setInterval(function(){ Carousel.Next(); },5000); // Cria o loop automático do carrossel
+
+                //Botão Próximo 
+                document.getElementById("next-btn").addEventListener("click", function(){Carousel.Next();});
+
+                //Botão Anterior
+                document.getElementById("prev-btn").addEventListener("click", function(){Carousel.Prev();});
+
             }
             
         } else {
@@ -35,33 +42,38 @@ class Carousel {
         }
     }
 
-    // Método responsável por trocar os slides
-    static Next(){
-        
-        // Pega o item atual do array
+    static Show(){
+
+         // Pega o item atual do array
         let item = carouselArr[Carousel._sequence];
 
-        // Seleciona a DIV da imagem no HTML
+         // Seleciona a DIV da imagem no HTML
         let carouselDiv = document.getElementById("carousel");
 
         // Seleciona a DIV do título no HTML
         let titleDiv = document.getElementById("carousel-title");
 
-
          // Define a imagem como fundo da DIV
         carouselDiv.style.backgroundImage = `url('img/${item.image}')`;
 
-        // Faz a imagem cobrir toda a área
+         // Faz a imagem cobrir toda a área
         carouselDiv.style.backgroundSize = "cover";
 
-          // Centraliza a imagem
+        // Centraliza a imagem
         carouselDiv.style.backgroundPosition = "center";
 
         // Garante transição suave (usa seu CSS)
-        carouselDiv.style.transition = "all .3s ease-in-out";
+        carouselDiv.style.transition = "all .5s ease-in-out";
 
-         // Injeta o título com link dentro da DIV
+        // Injeta o título com link dentro da DIV
         titleDiv.innerHTML = `<a href="${item.url}">${item.title}</a>`;
+
+
+    }
+
+
+    // Método responsável por trocar os slides
+    static Next(){
 
         // Avança para o próximo slide
         Carousel._sequence++;
@@ -70,7 +82,22 @@ class Carousel {
             Carousel._sequence = 0;
         }
 
-       
+       Carousel.Show();
 
     }
+
+    //Slide anterior
+    static Prev(){
+
+        Carousel._sequence--;
+
+        if(Carousel._sequence < 0){
+
+            Carousel._sequence =
+                Carousel._size - 1;
+        }
+
+        Carousel.Show();
+    }
+
 };
